40c1f0b2d32cc7b9e502090a0fa0299cb1b4f740
KenCaldeira <kcaldeira@carnegiescience.edu>
2023-02-17 12:25:53 -0800

    make test case and debug

--- utilities.py
+++ utilities.py
@@ -3,6 +3,12 @@ Utility functions for process_input
 """
 
 import xlrd
+import os
+
+"""
+read in first sheet of an xlsx file into an xlrd worksheet object
+"""
+def
 
 """
 read in first sheet of an excel file into an xlrd worksheet object
@@ -23,9 +29,9 @@ def remove_empty_rows(worksheet):
             worksheet._row_len.pop(row)
     return worksheet
 
-""""
+"""
 find first row of data in an xlrd worksheet that has a keyword in the first column, checking in a case insensitive manner
-""""
+"""
 def find_first_row_with_keyword(worksheet, keyword):
     for row in range(worksheet.nrows):
         if keyword.lower() in worksheet.cell_value(row, 0).lower():
@@ -53,22 +59,23 @@ create dictionary of allowable attributes for each component type
 """
 def create_component_attribute_dict(path_to_component_attributes, component_types):
     component_attribute_dict = {}
-    for component_type in :
+    for component_type in component_types:
         component_attribute_dict[component_type] = read_csv_file_to_list_of_attributes(path_to_component_attributes + component_type + '.csv')
 
 """
 list files in a directory, stripping out hidden files and eliminating file extension
 """
 def list_files_in_directory(directory):
-    return [f.split('.')[0] for f in listdir(directory) if isfile(join(directory, f)) and not f.startswith('.')]
+    return [f.split('.')[0] for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and not f.startswith('.')]
 
 """
-return true if all elements of a list are in any of the lists in a dictionary of lists, else return the elements that are not in any of the lists in the dictionary
+return true if all elements of a list are in any of the lists in a dictionary of lists or are empty strings or all spaces, else return the elements that are not in any of the lists in the dictionary
 """
-def check_if_all_elements_in_list_are_in_any_list_in_dict_of_lists(element_list, dict_of_lists):
+def check_if_all_elements_in_list_are_in_any_list_in_dict_of_lists_or_empty(element_list, dict_of_lists):
     for element in element_list:
-        if not any(element in dict_of_lists[key] for key in dict_of_lists):
-            return False, element
+        if element != '':
+            if not any(element in dict_of_lists[key] for key in dict_of_lists):
+                return False, element
     return True, None
 
 """
@@ -83,9 +90,9 @@ and a list of dictionaries from the 'tech_data' section
 """
 def read_excel_file_to_dict(file_name):
     # create dictionary of allowable attributes for each component type
-    component_directory = "../PyPSA/component_attributes/"
+    component_directory = "./PyPSA/pypsa/component_attrs/"
     component_types = list_files_in_directory(component_directory)
-    component_attribute_dict_list = create_component_attribute_dict(component_directory)
+    component_attribute_dict_list = create_component_attribute_dict(component_directory, component_types)
     # read in excel file describing case and technology data
     worksheet = read_excel_file(file_name)
     worksheet = remove_empty_rows(worksheet)
@@ -103,7 +110,7 @@ def read_excel_file_to_dict(file_name):
     attributes = tech_data.row_values(0)
     if(attributes[0].lower() != 'component'):
         raise Exception('First column of tech_data must be "component"')
-    good,bad_list = check_if_all_elements_in_list_are_in_any_list_in_dict_of_lists(attributes[1:], component_attribute_dict_list)
+    good,bad_list = check_if_all_elements_in_list_are_in_any_list_in_dict_of_lists_or_empty(attributes[1:], component_attribute_dict_list)
     if(good == False):
         raise Exception('Attributes in tech_data must be in the list of allowable attributes for the component type. Failed = '+concatenate_list_of_strings(bad_list))
     tech_data_list = []
@@ -114,8 +121,10 @@ def read_excel_file_to_dict(file_name):
             raise Exception('Component type in tech_data must be in the list of allowable component types. Failed = '+component)
         for i in range(1,len(row)):
             val = row[i].value
-            if(val != ''): # only add attribute to dictionary if it is not empty
+            if(val != '' and attribute[i] != '' ): # only add attribute to dictionary if it is not empty or attribute is not empty
                 if(attributes[i] in component_attribute_dict_list[component]):
                     tech_data_dict[attributes[i]] = val
         tech_data_list.append(tech_data_dict)
     return case_data_dict, tech_data_list
+
+case_dict,tech_list = read_excel_file_to_dict('test_case.xlsx')
