f39f5e04ab48a01ae495d71d6841415b6a161d3b
Bill Hayes <bhayes84@yahoo.com>
2023-04-17 10:48:06 -0700

    change filename to test_prefix_expected.xlsx

--- test/test_compare_output.py
+++ test/test_compare_output.py
@@ -7,26 +7,23 @@ print('cur dir:', os.getcwd())
 
 # find output_data path
 cur_dir = Path(os.getcwd())
-output_dir = str(cur_dir.joinpath('output_data'))
-
 output_test_case_dir = str(cur_dir.joinpath('output_data', 'test_case'))
-
 output_xlsx_path = str(cur_dir.joinpath('output_data', 'test_case', 'test_prefix.xlsx'))
 
 # check 3 'results' sheets
 was_error = False
 for sheet_name in ['case results', 'component results', 'time results']:
     output_df = pd.read_excel(output_xlsx_path, sheet_name=sheet_name)
-    expected_df = pd.read_excel('test/test_prefix.xlsx', sheet_name=sheet_name)
+    expected_df = pd.read_excel('test/test_prefix_expected.xlsx', sheet_name=sheet_name)
 
     # compare pypsa output to expected output
     comparison_df = output_df.compare(expected_df)
     print('='*40)
     if comparison_df.shape == (0,0):
-        print(f'OK, {sheet_name} output == expected')
+        print(f'OK, {sheet_name} sheet: output == expected')
         print('='*40)
     else:
-        print(f'ERROR: {sheet_name} output does not match expected output')
+        print(f'ERROR: {sheet_name} sheet: output does not match expected')
         print('  output df shape:  ', output_df.shape)
         print('  expected df shape:', expected_df.shape)
         print('Here are the differences:')
